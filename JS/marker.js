93cb3a36cc1d60216f340d2e0851c3f62c32c402
Mitfart <60539376+Mitfart@users.noreply.github.com>
2022-07-22 23:23:44 +0300

    22.07 70-80%
    -Main Nav
    -Main Slider (View)
    -Add 'Container'
    -Have a few idias how to implement slider

--- JS/marker.js
+++ JS/marker.js
@@ -1,61 +1,69 @@
 window.onload = function(){
-    const menuMarkerSelector = '#manu__marker';
-    const itemClass          = 'menu__item';
-    const activeItemClass    = 'menu__item_active';
-
-    const marker   = document.querySelector(menuMarkerSelector);
-    const items    = document.querySelectorAll('.' + itemClass);
-    let activeItem = document.querySelector('.' + activeItemClass);
+    const menuClass       = '.menu';
+    const markerClass     = menuClass + '__marker';
+    const itemClass       = menuClass + '__item';
+    const activeItemClass = menuClass + '__item_active';
 
 
-    let hoveredItem = activeItem;
-    let setHoverTO = null;
-    let setHoverToActiveTO = null;
+    const menus = document.querySelectorAll(menuClass);
 
-    const markerTransitionTime = window
-        .getComputedStyle(marker).transitionDuration
-        .replace('s', '') * 1000;
+    menus.forEach(menu => {
+        const marker   = menu.querySelector(markerClass);
+        if (marker == null) return;
 
+        const items    = menu.querySelectorAll(itemClass);
+        let activeItem = menu.querySelector   (activeItemClass);
 
-    function setIndicator(item) {
-        const pos      = Math.min(item.offsetLeft, hoveredItem.offsetLeft);
-        const deltaPos = Math.max(item.offsetLeft, hoveredItem.offsetLeft) - pos;
-        const width    = item.offsetWidth + deltaPos;
+        let hoveredItem = activeItem;
+        let setHoverTO = null;
+        let setHoverToActiveTO = null;
 
-        marker.style.left  = pos   + 'px';
-        marker.style.width = width + 'px';
-    }
+        setIndicator(activeItem);
 
-    function setActiveItem(item) {
-        activeItem.classList.remove(activeItemClass);
-        item.classList.add(activeItemClass);
-        activeItem = item;
-
-        setIndicator();
-    }
-
-    items.forEach(item => {
-        item.addEventListener('click', e => {
-            setActiveItem(e.target)
-        });
-        item.addEventListener('mouseover', e => {
-            clearTimeout(setHoverToActiveTO);
-            setIndicator(e.target);
-
-            setHoverTO = setTimeout(() => {
-                hoveredItem = e.target;
+        items.forEach(item => {
+            item.addEventListener('click', e => {
+                setActiveItem(e.target)
+            });
+            item.addEventListener('mouseover', e => {
+                clearTimeout(setHoverToActiveTO);
                 setIndicator(e.target);
-            }, markerTransitionTime)
-        });
-        item.addEventListener("mouseout", e => {
-            clearTimeout(setHoverTO);
 
-            setHoverToActiveTO = setTimeout(() => {
-                hoveredItem = activeItem;
-                setIndicator(activeItem);
-            }, markerTransitionTime)
+                setHoverTO = setTimeout(() => {
+                    hoveredItem = e.target;
+                    setIndicator(e.target);
+                }, markerTransitionTime)
+            });
+            item.addEventListener("mouseout", e => {
+                clearTimeout(setHoverTO);
+
+                setHoverToActiveTO = setTimeout(() => {
+                    hoveredItem = activeItem;
+                    setIndicator(activeItem);
+                }, markerTransitionTime)
+            });
         });
+
+
+        const markerTransitionTime = window
+            .getComputedStyle(marker).transitionDuration
+            .replace('s', '') * 1000;
+
+
+        function setIndicator(item) {
+            const pos      = Math.min(item.offsetLeft, hoveredItem.offsetLeft);
+            const deltaPos = Math.max(item.offsetLeft, hoveredItem.offsetLeft) - pos;
+            const width    = item.offsetWidth + deltaPos;
+
+            marker.style.left  = pos   + 'px';
+            marker.style.width = width + 'px';
+        }
+
+        function setActiveItem(item) {
+            activeItem.classList.remove(activeItemClass);
+            item.classList.add(activeItemClass);
+            activeItem = item;
+
+            setIndicator();
+        }
     });
-
-    setIndicator(activeItem);
 }
